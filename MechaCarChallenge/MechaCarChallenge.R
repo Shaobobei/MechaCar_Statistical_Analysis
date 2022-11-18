061c746a9981314be51cfed3ee92782e282a7e12
Zoey Liu <shaobobei@gmail.com>
2022-11-17 17:07:18 -0800

    updated

--- MechaCarChallenge/MechaCarChallenge.R
+++ MechaCarChallenge/MechaCarChallenge.R
@@ -17,12 +17,12 @@ Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFa
 Suspension_Coil
 
 ?total_summary()
-total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), median = median(PSI), varianc= var(PSI), standard_deviation = sd(PSI), .groups = 'keep')#create summary table with multiple columns
+total_summary <- Suspension_Coil %>% summarize( Mean = mean(PSI), Median = median(PSI), Varianc= var(PSI), Standard_Deviation = sd(PSI), .groups = 'keep')#create summary table with multiple columns
 total_summary 
 
 ?lot_summary()
 ?group_by()
-lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance= var(PSI), Standard_Deviation = sd(PSI), .groups = 'keep')
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize( Mean = mean(PSI), Median = median(PSI), Variance= var(PSI), Standard_Deviation = sd(PSI), .groups = 'keep')
 lot_summary
 
 ?t.test()
@@ -32,3 +32,4 @@ t.test(Suspension_Coil$PSI, mu=1500)
 t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
 t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
 t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
+
